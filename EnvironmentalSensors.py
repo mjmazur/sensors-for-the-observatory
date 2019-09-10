e971e301635ba9b1a2cf6cb2fa1dcdb9da5e99f0
mjmazur <mmazur5@uwo.ca>
2019-09-10 12:34:28 -0400

    Update EnvironmentalSensors.py

--- EnvironmentalSensors.py
+++ EnvironmentalSensors.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from time import gmtime, strftime, time
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
 
 def getvalues():
 
@@ -86,6 +87,7 @@ while True:
 		ax[0].set_ylim([np.amin(narray[1:,2])-5,np.amax(narray[1:,2])+5])
 		ax[0].set_xlim([0,24])
 		ax[0].xaxis.set_tick_params(labelbottom=True)
+		ax.xaxis.set_major_locator(MultipleLocator(1))
 		axst = ax[0].twinx()
 		axst.plot(x,narray[1:,3], color='red', label='Shed Temperature')
 		axst.set_ylabel('Shed Temperature (C)', size=15)
