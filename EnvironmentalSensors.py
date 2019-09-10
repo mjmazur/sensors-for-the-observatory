386b40d90b060699d81d1844e560a944a31a60b2
mjmazur <mmazur5@uwo.ca>
2019-09-10 12:23:10 -0400

    Update EnvironmentalSensors.py

--- EnvironmentalSensors.py
+++ EnvironmentalSensors.py
@@ -62,7 +62,7 @@ while True:
 	if hours > hoursold:
 		narray = np.vstack((narray, nline))
 		# f.write(str(time()) + ' ' + str(hours) + ' ' + str(line))
-		np.savetxt(write_path, narray, fmt='%.0f %.5f %.2f %.2f %.2f %.2f %.2f %.2f')
+		np.savetxt(write_path, narray, fmt='%d %.5f %.2f %.2f %.2f %.2f %.2f %.2f', header='Unix Time - Secs from 00 - Tfluid - Tshed - TF - HF - TG - HG')
 		#f.write(str(nline))
 		print(str(nline))
 		print('wrote line')
